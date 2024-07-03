34eaaa47d66766a5e15d736d34b02f277c6ed327
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-07-03 17:43:00 +0300

    Update main.dart

--- example3/lib/main.dart
+++ example3/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math' show pi;
+import 'package:vector_math/vector_math_64.dart' show Vector3;
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,14 +21,141 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  static const double widthAndHeight = 100;
+  late AnimationController _xController;
+  late AnimationController _yController;
+  late AnimationController _zController;
+  late Tween<double> _animation;
+
+  @override
+  void initState() {
+    _xController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 20,
+      ),
+    );
+
+    _yController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 30,
+      ),
+    );
+
+    _zController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 40,
+      ),
+    );
+
+    _animation = Tween(
+      begin: 0.0,
+      end: pi * 2,
+    );
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _xController.dispose();
+    _yController.dispose();
+    _zController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    _xController
+      ..reset()
+      ..repeat();
+
+    _yController
+      ..reset()
+      ..repeat();
+
+    _zController
+      ..reset()
+      ..repeat();
+
     return Scaffold(
       appBar: AppBar(),
-      body: Container(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100,
+              width: double.infinity,
+            ),
+            AnimatedBuilder(
+              animation: Listenable.merge(
+                [
+                  _xController,
+                  _yController,
+                  _zController,
+                ],
+              ),
+              builder: (context, child) => Transform(
+                transform: Matrix4.identity()
+                  ..rotateX(_animation.evaluate(_xController))
+                  ..rotateY(_animation.evaluate(_yController))
+                  ..rotateZ(_animation.evaluate(_zController)),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    // front
+                    Container(
+                      width: widthAndHeight,
+                      height: widthAndHeight,
+                      color: Colors.red,
+                    ),
+                    // back
+                    Container(
+                      width: widthAndHeight,
+                      height: widthAndHeight,
+                      color: Colors.amber,
+                      transform: Matrix4.identity()
+                        ..translate(
+                          Vector3(0, 0, -widthAndHeight),
+                        ),
+                    ),
+                    Container(
+                      width: widthAndHeight,
+                      height: widthAndHeight,
+                      color: Colors.purple,
+                    ),
+                    Container(
+                      width: widthAndHeight,
+                      height: widthAndHeight,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      width: widthAndHeight,
+                      height: widthAndHeight,
+                      color: Colors.teal,
+                    ),
+                    Container(
+                      width: widthAndHeight,
+                      height: widthAndHeight,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
