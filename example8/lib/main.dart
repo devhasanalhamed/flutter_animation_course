7a3bedacf95ab8d3e253c5a78054322f642ec881
Hasan Al-Hamed <therealhassan97@gmail.com>
2026-08-16 18:39:58 +0300

    Add 3D animated drawer and refactor app root
    Refactor app entry and add a 3D animated drawer. Renamed MyApp to App, switched to dark ThemeMode, and replaced the sample counter home with HomePage. Introduced MyDrawer (with TickerProviderStateMixin) that uses two AnimationControllers and Y-rotation/translation Matrix4 transforms to create a 3D drawer effect, driven by horizontal drag gestures. Added dart:math import and updated widget test to pump App. Changes affect example8/lib/main.dart and example8/test/widget_test.dart.

--- example8/lib/main.dart
+++ example8/lib/main.dart
@@ -1,121 +1,156 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MyDrawer(
+      drawer: Material(
+        child: Container(
+          color: const Color(0xFF24283B),
+          child: ListView.builder(
+            padding: const EdgeInsets.only(left: 80, top: 100),
+            itemCount: 20,
+            itemBuilder: (context, index) =>
+                ListTile(title: Text('Item $index')),
+          ),
+        ),
+      ),
+      child: Scaffold(appBar: AppBar(title: Text('3D Drawer'))),
+    );
+  }
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class MyDrawer extends StatefulWidget {
+  final Widget child;
+  final Widget drawer;
+  const MyDrawer({super.key, required this.child, required this.drawer});
+
+  @override
+  State<MyDrawer> createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<MyDrawer> with TickerProviderStateMixin {
+  late AnimationController xControllerForChild;
+  late Animation yRotationAnimationForChild;
+
+  late AnimationController xControllerForDrawer;
+  late Animation yRotationAnimationForDrawer;
+
+  @override
+  void initState() {
+    super.initState();
+    xControllerForChild = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+
+    yRotationAnimationForChild = Tween<double>(
+      begin: 0.0,
+      end: -pi / 2,
+    ).animate(xControllerForChild);
+
+    xControllerForDrawer = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+
+    yRotationAnimationForDrawer = Tween<double>(
+      begin: pi / 2.7,
+      end: 0.0,
+    ).animate(xControllerForDrawer);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    xControllerForChild.dispose();
+    xControllerForDrawer.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
+    final screenWidth = MediaQuery.of(context).size.width;
+    final maxDrag = screenWidth * 0.8;
+
+    return GestureDetector(
+      onHorizontalDragUpdate: (details) {
+        final delta = details.delta.dx / maxDrag;
+
+        xControllerForChild.value += delta;
+        xControllerForDrawer.value += delta;
+      },
+      onHorizontalDragEnd: (details) {
+        if (xControllerForChild.value < 0.5) {
+          xControllerForChild.reverse();
+          xControllerForDrawer.reverse();
+        } else {
+          xControllerForChild.forward();
+          xControllerForDrawer.forward();
+        }
+      },
+      child: AnimatedBuilder(
+        animation: Listenable.merge([
+          xControllerForChild,
+          xControllerForDrawer,
+        ]),
+        builder: (context, child) => Stack(
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Container(color: Color(0xFF1A1B26)),
+            Transform(
+              alignment: Alignment.centerLeft,
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.001)
+                ..translateByDouble(
+                  xControllerForChild.value * maxDrag,
+                  0.0,
+                  0.0,
+                  1.0,
+                )
+                ..rotateY(yRotationAnimationForChild.value),
+              child: widget.child,
+            ),
+            Transform(
+              alignment: Alignment.centerRight,
+
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.001)
+                ..translateByDouble(
+                  -screenWidth + xControllerForDrawer.value * maxDrag,
+                  0.0,
+                  0.0,
+                  1.0,
+                )
+                ..rotateY(yRotationAnimationForDrawer.value),
+              child: widget.drawer,
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
